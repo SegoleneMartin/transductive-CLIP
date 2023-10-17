38a4ca5f36228692ef1efafadf443af49b1f084c
SegoleneMartin <sego91140@hotmail.fr>
2023-10-17 06:39:33 +0000

    updated results and config

--- scripts/script_test_0shot.sh
+++ scripts/script_test_0shot.sh
@@ -6,25 +6,25 @@ acc_clustering=True
 device=0
 ######
 method='hard_em_dirichlet'
-for dataset in caltech101 food101 ucf101 
+for dataset in sun397
 do
-python main.py --opts batch_size 1000 number_tasks 10000 dataset ${dataset} shots ${s} n_query ${n_query} k_eff ${k_eff} save_results True device ${device} seed 0 method ${method} used_test_set ${used_test_set} acc_clustering ${acc_clustering}
+python main.py --opts batch_size 100 number_tasks 10000 dataset ${dataset} shots ${s} n_query ${n_query} k_eff ${k_eff} save_results True device ${device} seed 0 method ${method} used_test_set ${used_test_set} acc_clustering ${acc_clustering}
 done
 ######
 method='em_dirichlet'
-for dataset in caltech101 food101 ucf101 
+for dataset in sun397
 do
-python main.py --opts batch_size 1000 number_tasks 10000 dataset ${dataset} shots ${s} n_query ${n_query} k_eff ${k_eff} save_results True device ${device} seed 0 method ${method} used_test_set ${used_test_set} acc_clustering ${acc_clustering}
+python main.py --opts batch_size 100 number_tasks 10000 dataset ${dataset} shots ${s} n_query ${n_query} k_eff ${k_eff} save_results True device ${device} seed 0 method ${method} used_test_set ${used_test_set} acc_clustering ${acc_clustering}
 done
 ######
 method='kl_kmeans'
-for dataset in caltech101 food101 ucf101 
+for dataset in sun397
 do
-python main.py --opts batch_size 1000 number_tasks 10000 dataset ${dataset} shots ${s} n_query ${n_query} k_eff ${k_eff} save_results True device ${device} seed 0 method ${method} used_test_set ${used_test_set} acc_clustering ${acc_clustering}
+python main.py --opts batch_size 100 number_tasks 10000 dataset ${dataset} shots ${s} n_query ${n_query} k_eff ${k_eff} save_results True device ${device} seed 0 method ${method} used_test_set ${used_test_set} acc_clustering ${acc_clustering}
 done
 ######
 method='fuzzy_kmeans'
-for dataset in caltech101 food101 ucf101 
+for dataset in sun397
 do
-python main.py --opts batch_size 1000 number_tasks 10000 dataset ${dataset} shots ${s} n_query ${n_query} k_eff ${k_eff} save_results True device ${device} seed 0 method ${method} used_test_set ${used_test_set} acc_clustering ${acc_clustering}
+python main.py --opts batch_size 100 number_tasks 10000 dataset ${dataset} shots ${s} n_query ${n_query} k_eff ${k_eff} save_results True device ${device} seed 0 method ${method} used_test_set ${used_test_set} acc_clustering ${acc_clustering}
 done
